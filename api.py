a77594a32c2b3f1307ba3c974d13713efd1d344b
Arnav Chakravarty <arnav@Arnavs-MBP-2.cable.rcn.com>
2024-05-25 12:25:37 -0500

    add log returns function

--- api.py
+++ api.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
 
-class TimeSeriesApi:
 
+class TimeSeriesApi:
     def __init__(self, df: pd.DataFrame):
         self.df = df
 
     @property
     def df(self):
         return self._df
-    
+
     @df.setter
     def df(self, df):
         self._df = df
 
-    
+    def log_returns(self, col_names: list[str]):
+        for col_name in col_names:
+            self.df[f"{col_name}_log_return"] = np.log(
+                self.df[col_name] / self.df[col_name].shift(1)
+            )

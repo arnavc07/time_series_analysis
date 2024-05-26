f4764ff5243d794f5a27e5abdab256af192ad968
Arnav Chakravarty <arnavc07@gmail.com>
2024-05-25 23:08:14 -0500

    black

--- api.py
+++ api.py
@@ -29,12 +29,15 @@ class TimeSeriesApi:
     def cumulative_log_returns(self, col_names: list[str] = None):
         "given a list of columns, compute the cumulative log returns and add them to the DataFrame. If col_names is none, computes cumulative returns for all log return columns"
 
-        log_return_cols = [col_name for col_name in self.df.columns if "log_return" in col_name]
-        assert len(log_return_cols) > 0, "No log return columns found in the DataFrame, add log returns first"
+        log_return_cols = [
+            col_name for col_name in self.df.columns if "log_return" in col_name
+        ]
+        assert (
+            len(log_return_cols) > 0
+        ), "No log return columns found in the DataFrame, add log returns first"
 
         for col_name in log_return_cols:
             self.df[f"{col_name}_cumulative"] = self.df[col_name].cumsum()
-
 
     def plot(self, col_names: list[str], **kwargs):
         self.df[col_names].plot(**kwargs)
@@ -50,17 +53,17 @@ class TimeSeriesApi:
 
         for window in windows:
             for col_name in col_names:
-                self.df[f"{col_name}_sma_{window}"] = self.df[col_name].rolling(
-                    window=window
-                ).mean()
+                self.df[f"{col_name}_sma_{window}"] = (
+                    self.df[col_name].rolling(window=window).mean()
+                )
 
     def exponential_moving_average(self, col_names: list[str], windows: list[int]):
         "given a list of columns and a window size, compute the exponential moving average and add it to the DataFrame."
         for window in windows:
             for col_name in col_names:
-                self.df[f"{col_name}_ema_{window}"] = self.df[col_name].ewm(
-                    span=window, adjust=False
-                ).mean()
+                self.df[f"{col_name}_ema_{window}"] = (
+                    self.df[col_name].ewm(span=window, adjust=False).mean()
+                )
 
     def __call__(self):
         return self.df
